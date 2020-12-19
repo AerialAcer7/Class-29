343ea49b6eab857fc848ca896b763b13091dc1eb
AerialAcer7 <aerialace.shooter@gmail.com>
2020-12-19 16:54:15 +0530

    Catapult and Rubberband

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
         World.add(world, this.sling);
     }
 
@@ -19,9 +22,24 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            push();
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(66, 45, 24);
+            if(pointA.x<200){
+                image(this.sling3,pointA.x-25,pointA.y,10);
+                line(pointA.x-25, pointA.y+10, pointB.x+20, pointB.y-50);
+                line(pointA.x-25,pointA.y+10,pointB.x-20,pointB.y-50);
+            }
+            else{
+                image(this.sling3,pointA.x+25,pointA.y,10);   
+                line(pointA.x+25, pointA.y+10, pointB.x+20, pointB.y-50);
+                line(pointA.x+25,pointA.y+10,pointB.x-20,pointB.y-50);
+            }
+            pop();
         }
+
+       image(this.sling1,200,20);    
+       image(this.sling2,170,20);    
     }
     
 }
